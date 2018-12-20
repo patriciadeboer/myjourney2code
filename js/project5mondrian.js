4598013290e42bc976e566bbcdc6c95bb1d2de0d
Patricia de Boer <pdeboer05@gmail.com>
2018-12-19 18:49:54 -0600

    adding new functionality to Mondrian project 5

--- js/project5mondrian.js
+++ js/project5mondrian.js
@@ -12,11 +12,27 @@ $(document).ready(function () {
 		$('.masterpiece').css("visibility", "hidden")
 	};
 
-	var color1= ['pink','#9A4C95','mediumvioletred', '4A0D67','#551A8B', '#F08CAE', '#1D1A31', '#B68CB8','#FF00FF', '#754C78', '#FF1CAE'];
-	var color2= ['#388E8E', '#00EEEE', '#ADFC92', '#00F5FF','#0EBFE9','#98F5FF','#009ACD','#003EFF','#000080'];
-	var color3= ['lightcoral', 'coral', '#ff4602', '#CC1100', '#FDF0D5', '#EB5E55', '#2CF6B3', '#DE541E', '#FB4D3D'];
+	//OLD VERSION predetermined colors. Now truly random
+	// var color1= ['pink','#9A4C95','mediumvioletred', '4A0D67','#551A8B', '#F08CAE', '#1D1A31', '#B68CB8','#FF00FF', '#754C78', '#FF1CAE'];
+	// var color2= ['#388E8E', '#00EEEE', '#ADFC92', '#00F5FF','#0EBFE9','#98F5FF','#009ACD','#003EFF','#000080'];
+	// var color3= ['lightcoral', 'coral', '#ff4602', '#CC1100', '#FDF0D5', '#EB5E55', '#2CF6B3', '#DE541E', '#FB4D3D'];
 	var originalColors = ['yellow','red','blue'];
+
 	
+	function randomColor() {
+  	return (
+	    "rgba(" +
+	    Math.round(Math.random() * 250) +
+	    "," +
+	    Math.round(Math.random() * 250) +
+	    "," +
+	    Math.round(Math.random() * 250) +
+	    "," +
+	    Math.ceil(Math.random() * 10) / 10 +
+	    ")"
+	  );
+  	}
+
 	//Change color pallete
 	
 	$('.originalColors').on('click',setOriginalColors);
@@ -31,14 +47,27 @@ $(document).ready(function () {
 
 	//original colors reset
 	$('.changePalette').on('click',changePalette);
+	//OLD VERSION WITH PREDETERMINED COLORS
+	// function changePalette(){
+	// 	var randColor1 = Math.floor((Math.random()*color1.length));
+	// 	var randColor2 = Math.floor((Math.random()*color2.length));
+	// 	var randColor3 = Math.floor((Math.random()*color3.length));
+	// 	console.log(randColor2)
+	// 	$('.color1').css("background-color", color1[randColor1]);
+	// 	$('.color2').css("background-color", color2[randColor2]);
+	// 	$('.color3').css("background-color", color3[randColor3]);
+	// 	$('.boxes').css("background-color", "white");
+	// 	$('.masterpiece').css("visibility", "hidden");
+	// 	selectedColor='white';
+	// };
 	function changePalette(){
-		var randColor1 = Math.floor((Math.random()*color1.length));
-		var randColor2 = Math.floor((Math.random()*color2.length));
-		var randColor3 = Math.floor((Math.random()*color3.length));
+		var randColor1 = randomColor();
+		var randColor2 = randomColor();
+		var randColor3 = randomColor();
 		console.log(randColor2)
-		$('.color1').css("background-color", color1[randColor1]);
-		$('.color2').css("background-color", color2[randColor2]);
-		$('.color3').css("background-color", color3[randColor3]);
+		$('.color1').css("background-color", randColor1);
+		$('.color2').css("background-color", randColor2);
+		$('.color3').css("background-color", randColor3);
 		$('.boxes').css("background-color", "white");
 		$('.masterpiece').css("visibility", "hidden");
 		selectedColor='white';
