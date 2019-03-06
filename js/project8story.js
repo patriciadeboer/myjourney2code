2ff7f22a1225e7c64c5d069780342d1ce68fda8a
Gadget Chica <pdeboer05@gmail.com>
2019-03-05 22:28:12 -0600

    Updating project8 from mini

--- js/project8story.js
+++ js/project8story.js
@@ -1,23 +1,23 @@
 var promptsAdventure=[
 	'ADJECTIVE', 
 	'YOUR NAME',
-	'ADVERB ending in ly (such as quietly)']
-	// 'ADJECTIVE',
-	// 'ANIMAL (plural)',
-	// 'VERB ending in ING',
-	// 'ARTICLE of CLOTHING',
-	// 'FOOD',
-	// 'TYPE of LIQUOD',
-	// 'THING/OBJECT',
-	// 'VERB',
-	// 'PART of BODY'];
+	'ADVERB ending in ly (such as quietly)',
+	'ADJECTIVE',
+	'ANIMAL (plural)',
+	'VERB ending in ING',
+	'ARTICLE of CLOTHING',
+	'FOOD',
+	'TYPE of LIQUOD',
+	'THING/OBJECT',
+	'VERB',
+	'PART of BODY'];
 
 var answersAdventure=[];
 //Track current prompt
 var currentPrompt=0;
 //var currentAns=0;
 
-
+https://www.facebook.com/
 var nextPrompt=function(){
 	//adds input to answers array
 	if(currentPrompt != 0){
@@ -25,14 +25,28 @@ var nextPrompt=function(){
   	}
   	//if still prompts remaining, ask user
 	if(currentPrompt<promptsAdventure.length){
-		$('.prompt1').html('<input class="madlibInput" type="text"><br>'+ promptsAdventure[currentPrompt]);
+		$('.prompt1').html('<input class="madlibInput" id="madlibInputSpot" type="text"><br>'+ promptsAdventure[currentPrompt]);
 		currentPrompt+=1;
 	//otherwise show the final result/ madlib
 	}else{
 		showFinal();
 	}
 }
-
+//Try to see if changing ID to be different if I can get Enter key to work
+// var nextPrompt=function(){
+// 	//adds input to answers array
+// 	if(currentPrompt != 0){
+//     	answersAdventure.push($('.madlibInput').val());  
+//   	}
+//   	//if still prompts remaining, ask user
+// 	if(currentPrompt<promptsAdventure.length){
+// 		$('.prompt1').html('<input class="madlibInput" id="madlibInputSpot'+currentPrompt+'" type="text"><br>'+ promptsAdventure[currentPrompt]);
+// 		currentPrompt+=1;
+// 	//otherwise show the final result/ madlib
+// 	}else{
+// 		showFinal();
+// 	}
+// }
 
 //puts user answers into HTML
 var showFinal = function() {
@@ -54,6 +68,34 @@ var showFinal = function() {
 $(".madlibButton").click(function(){
   nextPrompt();
 });
-
 nextPrompt();
+
+
+
+
+
+var nextInput = document.getElementById("madlibInputSpot");
+nextInput.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    nextPrompt();
+  }
+});
+
+// var nextInput = document.getElementById("madlibInputSpot0");
+// nextInput.addEventListener("keyup", function(event) {
+//   if (event.keyCode === 13) {
+//     nextPrompt();
+//   }
+// });
+
+// var nextInput = document.getElementById("madlibInputSpot");
+
+// var onEnterKey = function(event){
+// 	if (event.keyCode === 13) {
+// 		nextPrompt();
+// 	}
+// };
+// $("#madlibInputSpot").addEventListener("keyup", onEnterKey);
+
+
 
